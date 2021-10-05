fb0bfca921803875c475ad036a9f4656da67666f
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-10-05 18:44:46 +0700

    Rename module/dist TableDataBundle-{,Perl-}CPAN-Release-Static-Older

---
+++ lib/TableData/Perl/CPAN/Release/Static/1997.pm
@@ -0,0 +1,49 @@
+package ## no critic: Modules::RequireFilenameMatchesPackage
+    # hide from PAUSE
+    TableDataRole::Perl::CPAN::Release::Static::1997;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny;
+with 'TableDataRole::Source::CSVInFile';
+
+around new => sub {
+    my $orig = shift;
+
+    require File::Basename;
+    my $filename = File::Basename::dirname(__FILE__) . '/../../../../../../share/1997.csv';
+    unless (-f $filename) {
+        require File::ShareDir;
+        $filename = File::ShareDir::dist_file('TableDataBundle-Perl-CPAN-Release-Static-Older', '1997.csv');
+    }
+    $orig->(@_, filename=>$filename);
+};
+
+package TableData::Perl::CPAN::Release::Static::1997;
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Role::Tiny::With;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+with 'TableDataRole::Perl::CPAN::Release::Static::1997';
+
+# STATS
+
+1;
+# ABSTRACT: CPAN releases for the year 1997
+
+=head1 TABLEDATA NOTES
+
+The data was retrieved from MetaCPAN.
+
+The C<status> column is the status of the release when the row was retrieved from
+MetaCPAN. It is probably not current, so do not use it.
---
+++ lib/TableDataBundle/Perl/CPAN/Release/Static/Older.pm
@@ -0,0 +1,26 @@
+# no code
+## no critic: TestingAndDebugging::RequireUseStrict
+package TableDataBundle::Perl::CPAN::Release::Static::Older;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Collection of TableData:: modules that contain older CPAN releases data
+
+=head1 DESCRIPTION
+
+This distribution contains the following L<TableData>:: modules:
+
+# INSERT_MODULES_LIST /^TableData::/
+
+
+=head1 SEE ALSO
+
+L<TableData>
+
+L<TableDataBundle::Perl::CPAN::Release>
+
+=cut
